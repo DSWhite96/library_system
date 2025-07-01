0dc505fb6f3a5d26e9149b7678eb36cece3d9c0a
Derrick White <derrickswhite@outlook.com>
2025-07-01 15:32:34 -0700

    Adding data to the database

--- server/lib_project/api/admin.py
+++ server/lib_project/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CatalogItem, Book, Game, Magazine, Movie, User
 
 # Register your models here.
+admin.site.register([CatalogItem, Book, Game, Magazine, Movie, User])
